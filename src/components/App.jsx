0f370ab0855c42ee572b09774d948f1b67ef3001
unknown <kononov8214@gmail.com>
2022-11-09 14:07:47 +0300

    refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
-// import styled from 'styled-components';
-
-
+import React from 'react';
+// import { Section } from './Section';
+// import { Statistics } from './Statistics';
 import FeedbackCounter from './Feedback/FeedbackCounter';
 
 
 
-export const App = () => {
-  return (
+export class App extends React.Component  {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+      } 
+      LeaveFedback = (event) => {
+        this.setState((prevState) => {
+            const { name } = event.target;
+            return {
+                [name]: prevState[name] + 1,            
+            }
+        })
+    }
 
-      <div
+  render () {
+  return(<div
         style={{
           height: '100vh',
           display: 'flex',
@@ -18,8 +31,27 @@ export const App = () => {
           color: '#010101'
         }}
         >
-        <FeedbackCounter />
+          {/* <Section title={'please leave feedback'}>
+          <FeedbackOptions
+                            options={[]} 
+                            onLeaveFeedback={}
+                            FeedbackCounter={this.counter} /> */}
+              <FeedbackCounter  good={this.state.good}
+                                bad={this.state.bad}
+                                neutral={this.state.neutral}
+                                LeaveFedback={this.LeaveFedback}/>
+          {/* </Section> */}
+          {/* <Section title={'statistics'}>
+                {total === 0 ? <Notification message={'There is no feedback'}/> :
+                    <Statistics
+                        good={positiveGrade} 
+                        neutral={neutralGrade } 
+                        bad={negativGrade}
+                        total={total}
+                        percentage={percentage}
+                            />}
+          </Section> */}
 
-      </div>
-  );
-};
+      </div>)
+  }
+}
--- src/components/Feedback/FeedbackCounter.jsx
+++ src/components/Feedback/FeedbackCounter.jsx
@@ -1,50 +1,35 @@
-import React from 'react';
-import { FeedbackControls } from './FeedbackControls';
+
+import { FeedbackOptions } from './FeedbackOptions';
 import { Statistics } from './Statistics';
 import { Section } from './Section';
 import { Notification } from './Notification';
 
-class FeedbackCounter extends React.Component{
-     
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-    }   
-        counter = (event) => {
-            this.setState((prevState) => {
-                const { name } = event.target;
-                return {
-                    [name]: prevState[name] + 1,            
-                }
-            })
-        }
+const FeedbackCounter = ({good,bad,neutral,LeaveFedback}) => {
+      
+        const total = good + neutral + bad;
+        const percentage =  Math.round((100 * good) / total)
         
-        render() {
-            const total = this.state.good + this.state.neutral + this.state.bad;
-            const percentage =  Math.round((100 * this.state.good) / total)
-            return (
+        return (
                 <div>
                     <Section title={'please leave feedback'}>
                         
-                        <FeedbackControls
-                            
-                            FeedbackCounter={this.counter} />
+                        <FeedbackOptions
+                            options={['good','neutral','bad']} 
+                            onLeaveFeedback={LeaveFedback} />
                     </Section>
                     <Section title={'statistics'}>
                     {total === 0 ?   <Notification message={'There is no feedback'}/> :
                     <Statistics
-                        good={this.state.good } 
-                        neutral={this.state.neutral } 
-                        bad={this.state.bad}
+                        good={good} 
+                        neutral={neutral} 
+                        bad={bad}
                         total={total}
                         percentage={percentage}
                             />}
                     </Section>
 
             </div>
-        )
-    }
+    )
 }
 
 export default FeedbackCounter;
